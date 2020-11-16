f1a418d997d51c17991ea9a194d537724edf3ea7
Maksood Mohiuddin <maksood@gmail.com>
2020-11-16 16:48:53 -0500

    Container and Content initial orchestration

---
+++ react-docker-container/build/precache-manifest.92852210110ebe2729bd5e9a7b6387fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc4d3511a77c0e722c6215629062f8b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7192ae43bdeb9a0126ef",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0cdc8c5e7e03d0c86618",
+    "url": "/static/js/2.3cb50d00.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3cb50d00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7192ae43bdeb9a0126ef",
+    "url": "/static/js/main.32583db1.chunk.js"
+  },
+  {
+    "revision": "c5cff9443435d5a8c56d",
+    "url": "/static/js/runtime-main.559635c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
